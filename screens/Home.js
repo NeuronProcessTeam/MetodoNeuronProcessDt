323ea8e54af018d80116a98e7777c536a95f59a3
Cristianvelazquez <cristian.py.isc@gmail.com>
2021-03-12 18:19:33 -0600

    Modificaciones en contenido

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,61 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions } from 'react-native'
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
 
 export default function Home() {
     return (
-        <View>
-            <Text> Hola soy un Home</Text>
-        </View>
+        <View style={styles.container}>
+            <ScrollView style={styles.ScrollView}>
+                <Image style={styles.Image}
+                source={require('../assets/image/porque.jpg')}
+                ></Image>
+                <Text style={styles.Titulo}>¿PORQUE EL METODO NEURON PROCESS?</Text>
+                <Text style={styles.parrafo}>Desarrollo para nuestra cultura por expertos en 
+                procesos creativos divertido, informal para resolver problemas</Text>
+                
+                
+                <Image style={styles.Image}
+                source={require('../assets/image/quees.jpg')}>
+                </Image>
+                <Text style={styles.Titulo}>¿QUE ES EL METODO NEURON PROCESS?</Text>
+                <Text style={styles.parrafo}>El metodo que hemos incubado para resolver problemas
+                Utiliza herramientas de desing thinking. Evoluciona constantemente</Text>
+
+                <Text style={styles.Titulo}>ACERCA DE...</Text>
+                <Image style={styles.Image}
+                source={require('../assets/image/neuron.jpg')}></Image>
+                <Text style={styles.parrafo}>Somos un grupo de jóvenes profesionales
+                que busca usar el metodo Neuron para resolver tus preocupaciones
+                de forma creativa</Text>
+            </ScrollView>
+        </View>    
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ScrollView: {
+        width: Dimensions.get('window').width,
+    },
+    Image: {
+        width: 410,
+        height: 300,
+        marginTop: '5%',
+        marginBottom: '10%'
+    },
+    Titulo: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginBottom: '5%'
+    },
+    parrafo: {
+        fontSize: 15,
+        textAlign: 'center',
+        marginBottom: '4%'
+    }
+
+})
